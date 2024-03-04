2e5e59174e1b1f8e394ee25cb0963f041e719160
Yezuz <jesus.antonio.fuentes.lizama@uabc.edu.mx>
2024-03-03 22:07:47 -0600

    fixed close buffer mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,4 +8,9 @@ return {
   vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv"),
   vim.keymap.set('n', '<c-n>', '<Cmd>Neotree toggle<CR>'),
   vim.keymap.set('n', '<C-i>', '<Cmd>BufferNext<CR>', { silent = true }),
+  vim.keymap.set('n', '<C-c>', '<Cmd>BufferClose<CR>'),
+  -- CSharp
+  vim.keymap.set('n', 'cgd', function()
+    require('csharp').go_to_definition()
+  end),
 }
